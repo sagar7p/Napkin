39f77165f6c97892be8aa259fb3dd10e065b0c42
SagarPunhani <sagar7p@gmail.com>
2016-01-15 22:00:25 -0800

    First Commit

--- Nap/SecondViewController.swift
+++ Nap/SecondViewController.swift
@@ -12,6 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hello")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
